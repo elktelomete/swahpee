d0ce31c62337d3958cb5926829ea081e40c550c8
Bastian Ruz <bastian.ruz@visma.com>
2022-02-25 16:01:39 +0100

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/button';
 
 function App() {
   return (
-    <>
     <div className='bodyDiv'>
       <header className='cabeza'>
-        <h6 style={{color: 'white'}} >didn du nuttin</h6>
+          <h6 style={{ color: 'white' }} >didn du nuttin</h6>
+          <Button variant="primary">caca</Button>
       </header>
-        <body>
+        <body><span>
           
+        </span>
       </body>
     </div>
-    </>
   );
 }
 
